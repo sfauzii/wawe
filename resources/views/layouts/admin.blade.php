c2e0fc632dfaedb47490c7e9276d248bc7fcdd85
sfauzii <sjahrulfauzii@gmail.com>
2024-04-27 20:41:59 +0700

    feat(admin-panels): admin panels template integration

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    @include('includes.admin.style')
+
+    <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Apr 20 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top d-flex align-items-center">
+
+        @include('includes.admin.navbar')
+
+    </header><!-- End Header -->
+
+    <!-- ======= Sidebar ======= -->
+    @include('includes.admin.sidebar')
+    <!-- End Sidebar-->
+
+    <main id="main" class="main">
+
+        @yield('content')
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    @include('includes.admin.footer')
+    <!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    @include('includes.admin.script')
+
+</body>
+
+</html>
