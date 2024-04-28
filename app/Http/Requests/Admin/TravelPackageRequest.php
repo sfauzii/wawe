97eabdc82a3b2f9f271c5adfa599e6d89372db73
sfauzii <sjahrulfauzii@gmail.com>
2024-04-28 10:50:04 +0700

    refactor(crud TravelPackage): form request validation

---
+++ app/Http/Requests/Admin/TravelPackageRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TravelPackageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:255',
+            'location' => 'required|max:255',
+            'about' => 'required',
+            'departure_date' => 'required|date',
+            'duration' => 'required|max:255',
+            'type' => 'required|max:255',
+            'price' => 'required|integer',
+        ];
+    }
+}
