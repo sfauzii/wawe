8f16c580877c582c4c0aafa1c8c27e3d61f8f688
sfauzii <sjahrulfauzii@gmail.com>
2024-04-28 08:01:12 +0700

    feat(migrations): create migrations travel_package

---
+++ database/migrations/2024_04_28_005304_create_travel_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('travel_packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('location');
+            $table->longText('about');
+            $table->date('departure_date');
+            $table->string('duration');
+            $table->string('type');
+            $table->integer('price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travel_packages');
+    }
+};
