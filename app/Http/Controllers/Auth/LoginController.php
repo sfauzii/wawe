4981bc54519ec0b92e95ef39b6aa40551419b18e
sfauzii <sjahrulfauzii@gmail.com>
2024-04-28 16:27:51 +0700

    refactor(auth): fix redirect when logging in as admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,17 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        // Check if user has 'ADMIN' role
+        if ($user->roles === 'ADMIN') {
+            // Redirect to admin dashboard
+            return redirect()->route('dashboard');
+        }
+
+        // For other roles, redirect to default home page
+        return redirect('/');
     }
 }
