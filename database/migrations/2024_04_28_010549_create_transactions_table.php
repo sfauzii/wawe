f793611efb88d7feb486daab73ede8a7404e7cc2
sfauzii <sjahrulfauzii@gmail.com>
2024-04-28 08:10:17 +0700

    feat(migrations): create migrations transactions

---
+++ database/migrations/2024_04_28_010549_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('travel_packages_id');
+            $table->integer('users_id')->nullable();
+            $table->integer('additional_visa');
+            $table->integer('transaction_total');
+            $table->string('transaction_status'); //IN_CART, PENDING, SUCCESS, CANCAEL, FAILED
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
