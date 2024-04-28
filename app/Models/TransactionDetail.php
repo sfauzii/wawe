a8abce375506566ec2ef52b1b5d5cd56e3a4f2d9
sfauzii <sjahrulfauzii@gmail.com>
2024-04-28 13:39:18 +0700

    refactor(crud Transaction): model TransactionDetail

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Transaction;
+use App\Models\TravelPackage;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TransactionDetail extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable =[
+        'transactions_id', 'username', 'nationality', 'is_visa', 'doe_passport'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function transaction(){
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id'); 
+    }
+
+    public function travel_package()
+    {
+        return $this->belongsTo(TravelPackage::class, 'travel_packages_id');
+    }
 }
