c31196e6f77e639666f5d7f82184925957862a8b
sfauzii <sjahrulfauzii@gmail.com>
2024-05-04 14:36:13 +0700

    first commit

--- resources/views/pages/admin/transaction/detail.blade.php
+++ resources/views/pages/admin/transaction/detail.blade.php
@@ -29,7 +29,7 @@
                 <div class="card">
                     <div class="card-body">
 
-                        <table class="table table-bordered">
+                        <table class="table table-bordered mt-4">
                             <tr>
                                 <th>ID</th>
                                 <td>{{ $item->id }}</td>
