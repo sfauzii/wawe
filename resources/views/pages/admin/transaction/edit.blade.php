c9fcbcc03b79cd25ed27bce7f1583138969a66e0
sfauzii <sjahrulfauzii@gmail.com>
2024-05-04 12:53:17 +0700

    refactor(admin-panels): Add last information in edit menu

--- resources/views/pages/admin/transaction/edit.blade.php
+++ resources/views/pages/admin/transaction/edit.blade.php
@@ -24,7 +24,7 @@
 
     <section class="section">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-8">
 
                 <div class="card">
                     <div class="card-body">
@@ -61,7 +61,42 @@
 
             </div>
 
+            <div class="col-lg-4">
 
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Last Information</h5>
+
+                        <!-- Advanced Form Elements -->
+                        <form>
+                            <!-- Created At -->
+                            <div class="row mb-5">
+                                <div class="col-sm-10">
+                                    <div class="d-flex align-items-center">
+                                        <i class="fas fa-calendar-alt mr-2"></i> <!-- Icon untuk tanggal -->
+                                        <span >Created At: <span style="color: #012970; font-weight: bold   ">{{ $item->created_at->setTimezone('Asia/Jakarta')->format('j F Y, H:i T')  }}</span></span>
+                                        <!-- Tanggal yang sesuai akan ditampilkan di sini -->
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Updated At -->
+                            <div class="row mb-5">
+                                <div class="col-sm-10">
+                                    <div class="d-flex align-items-center">
+                                        <i class="fas fa-calendar-check mr-2"></i> <!-- Icon untuk tanggal -->
+                                        <span >Updated At: <span style="color: #012970; font-weight: bold   ">{{ $item->updated_at->setTimezone('Asia/Jakarta')->format('j F Y, H:i T')  }}</span></span>
+                                        <!-- Tanggal yang sesuai akan ditampilkan di sini -->
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <!-- End General Form Elements -->
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     </section>
 @endsection
