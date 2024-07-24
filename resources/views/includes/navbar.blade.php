38fb15dfd431eac8a89cf77e57fb58ba32706ee5
sfauzii <sjahrulfauzii@gmail.com>
2024-07-24 14:31:24 +0700

    refactor(frontend): fixed navbar frontend

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -14,7 +14,7 @@
                     <a href="#" class="nav-link active">Home</a>
                 </li>
                 <li class="nav-item mx-md-2">
-                    <a href="{{ route('catalog') }}" class="nav-link">Paket Healing</a>
+                    <a href="{{ route('catalog') }}" class="nav-link">Catalog</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">
@@ -36,26 +36,52 @@
             @endguest
 
             <!-- Profil untuk mobile di dalam collapse -->
-            <div class="navbar-profile-mobile d-lg-none mt-3 text-center">
-                <img src="{{ url('frontend/images/member.png') }}" alt="Profile Photo" class="profile-photo" />
-                <span class="profile-name ml-2">John Doe</span>
-            </div>
+            @auth
+                <div class="navbar-profile-mobile d-lg-none mt-3 text-center">
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButtonMobile"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="{{ Auth::user()->photo ? asset('storage/' . Auth::user()->photo) : 'https://ui-avatars.com/api/?name=' . Auth::user()->name }}"
+                                alt="Profile Photo" class="profile-photo" />
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButtonMobile">
+                            <!-- Dropdown content -->
+                            <a class="dropdown-item"
+                                href="{{ route('overview', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}">Overview</a>
+                            <a class="dropdown-item"
+                                href="{{ route('overview', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}">My
+                                Ticket</a>
+                            <a class="dropdown-item" href="#">Settings</a>
+                            <div class="dropdown-divider"></div>
+                            <form action="{{ url('logout') }}" method="POST">
+                                @csrf
+                                <button class="dropdown-item" type="submit">Logout</button>
+                            </form>
+                        </div>
+                    </div>
+                    <span class="profile-name ml-2">Hi, {{ ucfirst(Auth::user()->name) }}</span>
+                </div>
+            @endauth
         </div>
         <!-- Tambahkan elemen profil di sini -->
         @auth
             <div class="navbar-profile d-none d-lg-flex align-items-center ml-auto">
-                <span class="profile-name ml-2">John Doe</span>
+                <span class="profile-name ml-2">Hi, {{ ucfirst(Auth::user()->name) }}</span>
                 <div class="dropdown">
                     <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
-                        <img src="{{ url('frontend/images/member.png') }}" alt="Profile Photo" class="profile-photo" />
+                        <img src="{{ Auth::user()->photo ? asset('storage/' . Auth::user()->photo) : 'https://ui-avatars.com/api/?name=' . Auth::user()->name }}"
+                            alt="Profile Photo {{ Auth::user()->name }}" class="profile-photo" />
                     </button>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                         <!-- Konten dropdown -->
                         <a class="dropdown-item"
                             href="{{ route('overview', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}">Overview</a>
-                        <a class="dropdown-item" href="dashboard.html">My Ticket</a>
-                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item"
+                            href="{{ route('my-ticket', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}">My
+                            Ticket</a>
+                        <a class="dropdown-item"
+                            href="{{ route('settings', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}">Settings</a>
                         <div class="dropdown-divider"></div>
                         <form action="{{ url('logout') }}" method="POST">
                             @csrf
