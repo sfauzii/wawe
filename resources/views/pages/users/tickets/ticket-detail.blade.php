5e220b9222f910bd1796c4e0a40d1aca1697bfdb
sfauzii <sjahrulfauzii@gmail.com>
2024-10-05 14:10:22 +0700

    refactor(frontend-ticket-details): fixed Changes to ticket details page styling

--- resources/views/pages/users/tickets/ticket-detail.blade.php
+++ resources/views/pages/users/tickets/ticket-detail.blade.php
@@ -5,93 +5,81 @@
 @endsection
 
 @section('content')
+
     <main>
-        <div class="card-container">
-            <div class="custom-card">
-                <div class="card-content">
-                    <div class="product-info">
-                        <div class="product-image">
-                            {{-- <img src="{{ asset('storage/' . $item->travel_package->galleries[0]->image) }}" alt="{{ $item->travel_package->title }}"> --}}
-                            @if ($item->travel_package->galleries->isNotEmpty())
-                                @php
-                                    // Get the first gallery item
-                                    $firstGallery = $item->travel_package->galleries->first();
+        <div class="container">
+            <div class="title-section">
+                <h1>Your Tickets</h1>
+                <p>Unduh tiket Anda di sini untuk ditukarkan dengan tiket fisik saat acara berlangsung.</p>
+            </div>
 
-                                    // Ensure image is an array and get the first image path
-                                    $firstImagePath = is_array($firstGallery->image)
-                                        ? $firstGallery->image[0]
-                                        : $firstGallery->image;
-                                @endphp
+            <div class="card-container">
+                <div class="card-header">
+                    @if ($item->travel_package->galleries->isNotEmpty())
+                        @php
+                            $firstGallery = $item->travel_package->galleries->first();
 
-                                @if ($firstImagePath)
-                                    <img src="{{ asset('storage/' . $firstImagePath) }}"
-                                        alt="{{ ucwords($item->travel_package->title) }}">
-                                @else
-                                    <!-- Fallback content if there is no image -->
-                                    <p>No image available.</p>
-                                @endif
-                            @else
-                                <!-- Fallback content if there are no galleries -->
-                                <p>No galleries available.</p>
-                            @endif
-                        </div>
+                            $firstImagePath = is_array($firstGallery->image)
+                                ? $firstGallery->image[0]
+                                : $firstGallery->image;
+                        @endphp
 
-                        <div class="product-details">
-                            <h2 class="product-title">{{ ucwords($item->travel_package->title) }}</h2>
-                            <div class="product-status">
-                                <p>{{ $item->transaction_status }}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="icon-container">
-                        <span class="icon">
-                            <!-- <ion-icon name="calendar-outline"></ion-icon> -->
-                            <img src="{{ url('frontend/images/icons/calendar.png') }}" alt="Check Icon" class="icon-img-succes">
-                            <!-- Ganti dengan ikon Font Awesome yang sesuai -->
-                        </span>
-                        <div class="icon-description">
-                            <p>{{ \Carbon\Carbon::create($item->travel_package->departure_date)->format('d F Y') }}</p>
-                        </div>
-                        <span class="icon">
-                            <!-- <ion-icon name="people-outline">Ganti dengan ikon Font Awesome yang sesuai -->
-                            <img src="{{ url('frontend/images/icons/people.png') }}" alt="Check Icon" class="icon-img-succes">
-                        </span>
-                        <div class="icon-description">
-                            <p>{{ $transactionDetails->count() }} person</p>
-                        </div>
-                        <span class="icon">
-                            <!-- <ion-icon name="pricetag-outline"></ion-icon>Ganti dengan ikon Font Awesome yang sesuai -->
-                            <img src="{{ url('frontend/images/icons/tag.png') }}" alt="Check Icon" class="icon-img-succes">
-                        </span>
-                        <div class="icon-description">
-                            <p>{{ ucfirst($item->travel_package->type) }}</p>
-                        </div>
-                        <span class="icon">
-                            <!-- <ion-icon name="cash-outline"></ion-icon> -->
-                            <!-- Ganti dengan ikon Font Awesome yang sesuai -->
-
-                            <img src="{{ url('frontend/images/icons/dollar-circle.png') }}" alt="Check Icon" class="icon-img-succes">
-                        </span>
-                        <div class="icon-description">
-                            <p>IDR {{ number_format($item->transaction_total, 0, ',') }}</p>
-                        </div>
-                    </div>
-                    <hr class="divider">
-                    <div class="description">
-                        <h3>What's Next?</h3>
-                        <p>Silahkan untuk bergabung pada <strong class="mt-5"><a href="https://chat.whatsapp.com/KMK91AjwdCfGkaENT4R9xp" target="_blank">Group Whatsapp</a></strong></p>
-                    </div>
-                    <div class="buttons">
-                        <button class="action-button"
-                            onclick="window.location.href = '{{ route('testimony.create', $item->id) }}';">Testimonies</button>
-                        <button class="action-button-secondary" onclick="window.location.href = '{{ route('ticket-download', $item->id) }}';">Download Ticket</button>
+                        @if ($firstImagePath)
+                            <img src="{{ asset('storage/' . $firstImagePath) }}"
+                                alt="{{ ucwords($item->travel_package->title) }}" class="profile-img">
+                        @else
+                            <p>No image available.</p>
+                        @endif
+                    @else
+                        <p>No galleries available.</p>
+                    @endif
+                    <div class="header-info">
+                        <h2>{{ ucwords($item->travel_package->title) }}</h2>
+                        <span class="badge">{{ $item->transaction_status }}</span>
                     </div>
                 </div>
+
+                <div class="icon-list">
+                    <div class="icon-item">
+                        <img src="{{ url('frontend/images/icons/calendar.png') }}" alt="Calendar Icon" class="icon">
+                        <p>{{ \Carbon\Carbon::create($item->travel_package->departure_date)->format('d F Y') }}</p>
+                    </div>
+                    <div class="icon-item">
+                        <img src="{{ url('frontend/images/icons/people.png') }}" alt="People Icon" class="icon">
+                        <p>{{ $transactionDetails->count() }} person</p>
+                    </div>
+                    <div class="icon-item">
+                        <img src="{{ url('frontend/images/icons/tag.png') }}" alt="Tag Icon" class="icon">
+                        <p>{{ ucwords($item->travel_package->type) }}</p>
+                    </div>
+                    <div class="icon-item">
+                        <img src="{{ url('frontend/images/icons/dollar-circle.png') }}" alt="Price Icon" class="icon">
+                        <p>Rp {{ number_format($item->grand_total, 0, ' ') }}</p>
+                    </div>
+                </div>
+
+                <hr class="divider">
+
+                <div class="description">
+                    <h3>What's Next?</h3>
+                    <p>Silahkan untuk bergabung pada <strong class="mt-5"><a
+                                href="https://chat.whatsapp.com/KMK91AjwdCfGkaENT4R9xp" target="_blank">Group
+                                Whatsapp</a></strong></p>
+                    </p>
+                </div>
+
+                <div class="button-container">
+                    <button class="action-button"
+                        onclick="window.location.href = '{{ route('testimony.create', $item->id) }}';">Testimonies</button>
+                    <button class="action-button-secondary"
+                        onclick="window.location.href = '{{ route('ticket-download', $item->id) }}';">Download
+                        Ticket</button>
+                </div>
             </div>
+
+            <footer>
+                <p>&copy; 2024 WaWe Tour and Travel. All rights reserved.</p>
+            </footer>
         </div>
-        <!-- Footer -->
-        <footer>
-            <p>&copy; 2024 WaWe Tour and Travel. All rights reserved.</p>
-        </footer>
     </main>
 @endsection
