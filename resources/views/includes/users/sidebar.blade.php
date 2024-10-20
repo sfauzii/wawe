f311914f41c27a7b7975d1aaf2bcc61207c01e39
sfauzii <sjahrulfauzii@gmail.com>
2024-10-20 15:11:54 +0700

    refactor(frontend-sidebar): fixed logo in sidebar dashboard users

--- resources/views/includes/users/sidebar.blade.php
+++ resources/views/includes/users/sidebar.blade.php
@@ -7,33 +7,37 @@
     <div class="top">
         <div class="logo">
             <!-- <h3><span class="danger">Wawe</span></h3> -->
-            <a href="{{ route('home') }}"><img src="{{ url('frontend/images/logo.png') }}" alt="Logo WaWe"></a>
+            <a href="{{ route('home') }}"><img src="{{ url('frontend/svg/images/logo.svg') }}" alt="Logo WaWe"></a>
         </div>
         <div class="close" id="close_btn">
             <span class="material-icons">close</span>
         </div>
     </div>
     <div class="sidebar">
-        <a href="{{ route('overview', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}" class="{{ $currentUrl == route('overview', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
+        <a href="{{ route('overview', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}"
+            class="{{ $currentUrl == route('overview', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
             <!-- <span class="material-icons">grid_view</span> -->
 
             <img src="{{ url('frontend/images/icons/category.png') }}" class="icon-sidebar">
             <h3>Overview</h3>
         </a>
-        <a href="{{ route('my-ticket', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}" class="{{ $currentUrl == route('my-ticket', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
+        <a href="{{ route('my-ticket', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}"
+            class="{{ $currentUrl == route('my-ticket', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
             <!-- <span class="material-icons">receipt_long</span> -->
-             <img src="{{ url('frontend/images/icons/ticket-discount.png') }}" class="icon-sidebar">
+            <img src="{{ url('frontend/images/icons/ticket-discount.png') }}" class="icon-sidebar">
             <h3>My Ticket</h3>
         </a>
-        <a href="{{ route('my-transaction', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}" class="{{ $currentUrl == route('my-transaction', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
+        <a href="{{ route('my-transaction', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}"
+            class="{{ $currentUrl == route('my-transaction', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
             <!-- <span class="material-icons">payments</span> -->
-             <img src="{{ url('frontend/images/icons/empty-wallet.png') }}" class="icon-sidebar">
+            <img src="{{ url('frontend/images/icons/empty-wallet.png') }}" class="icon-sidebar">
             <h3>My Transaction</h3>
         </a>
 
-        <a href="{{ route('settings', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}" class="{{ $currentUrl == route('settings', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
+        <a href="{{ route('settings', ['username' => Auth::user()->username, 'id' => Auth::id()]) }}"
+            class="{{ $currentUrl == route('settings', ['username' => Auth::user()->username, 'id' => Auth::id()]) ? 'active' : '' }}">
             <!-- <span class="material-icons">settings</span> -->
-             <img src="{{ url('frontend/images/icons/setting-3.png') }}" class="icon-sidebar" >
+            <img src="{{ url('frontend/images/icons/setting-3.png') }}" class="icon-sidebar">
             <h3>Settings</h3>
         </a>
     </div>
