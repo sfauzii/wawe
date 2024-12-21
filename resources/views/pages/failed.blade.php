69c55a8010e2ba4097bc845a2f666e5f76eba34e
sfauzii <sjahrulfauzii@gmail.com>
2024-12-21 11:45:12 +0700

    refactor: :lipstick: fixed page unfinish and failed notification payment

--- resources/views/pages/failed.blade.php
+++ resources/views/pages/failed.blade.php
@@ -3,21 +3,20 @@
 @section('title', 'Checkout Failed')
 
 @section('content')
-<main>
-    <div class="section-success d-flex align-items-center">
-        <div class="col text-center">
-            <img src="{{ url('frontend/images/success.png') }}" alt="" style="width: 300px; height: auto;">
-            <h1>Oopss!</h1>
-            <p>
-                Your transaction is failed
-                <br>
-                Please contact our representative if this problem occurs
-            </p>
-            <a href="{{ url('/')}}" class="btn btn-home-page mt-3 px-5">
-                Home Page
-            </a>
+    <main>
+        <div class="section-success d-flex align-items-center">
+            <div class="col text-center">
+                <img src="{{ url('frontend/images/success.png') }}" alt="" style="width: 300px; height: auto;">
+                <h1>Oopss!</h1>
+                <p>
+                    Your transaction is failed
+                    <br>
+                    Please contact our representative if this problem occurs
+                </p>
+              
+                <button class="get-started-button" onclick="window.location.href = 'https://wa.me/6285786192909?text=Halo%2C+saya+ingin+bantuan+tentang+transaksi+saya.';">Contact Us</button>
+            </div>
         </div>
-    </div>
-</main>
-    
+    </main>
+
 @endsection
--- resources/views/pages/unfinish.blade.php
+++ resources/views/pages/unfinish.blade.php
@@ -3,21 +3,21 @@
 @section('title', 'Checkout Success')
 
 @section('content')
-<main>
-    <div class="section-success d-flex align-items-center">
-        <div class="col text-center">
-            <img src="{{ url('frontend/images/success.png') }}" alt="" style="width: 300px; height: auto;">
-            <h1>Oops!</h1>
-            <p>
-                Your transaction is unfinished.
-                <br>
-                please read it as well
-            </p>
-            <a href="{{ url('/')}}" class="btn btn-home-page mt-3 px-5">
-                Home Page
-            </a>
+    <main>
+        <div class="section-success d-flex align-items-center">
+            <div class="col text-center">
+                <img src="{{ url('frontend/images/success.png') }}" alt="" style="width: 300px; height: auto;">
+                <h1>Oops!</h1>
+                <p>
+                    Your transaction is unfinished.
+                    <br>
+                    please read it as well
+                </p>
+                <button class="get-started-button"
+                    onclick="window.location.href = 'https://wa.me/6285786192909?text=Halo%2C+saya+ingin+bantuan+tentang+transaksi+saya.';">Contact
+                    Us</button>
+            </div>
         </div>
-    </div>
-</main>
-    
+    </main>
+
 @endsection
