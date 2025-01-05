0f68d71d2034e34b19aef5e46f484f38e7b37bab
sfauzii <sjahrulfauzii@gmail.com>
2025-01-05 16:00:33 +0700

    refactor: :lipstick: fixed font google

--- resources/views/layouts/auth-alternative.blade.php
+++ resources/views/layouts/auth-alternative.blade.php
@@ -16,7 +16,7 @@
         <!-- Google Fonts -->
         <link href="https://fonts.gstatic.com" rel="preconnect">
         <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet">
 
         <!-- Vendor CSS Files -->
